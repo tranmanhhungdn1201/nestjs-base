7b8daa72512eba5841d3f91bd62caad8e543ad66
tranmanhhung <hung.tran@enablestartup.com>
2023-05-10 10:13:35 +0700

    serializing response interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
-import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
-import { ExceptionsLoggerFilter } from './utils/exceptionsLogger.filter';
-import { ValidationPipe } from '@nestjs/common';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { ExcludeNullInterceptor } from './utils/excludeNull.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalFilters(new ExceptionsLoggerFilter(httpAdapter));
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(
+    app.get(Reflector))
+  );
+  app.useGlobalInterceptors(new ExcludeNullInterceptor());
   app.use(cookieParser());
   await app.listen(3000);
 }
